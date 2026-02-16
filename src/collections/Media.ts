242d550005535a675b91115e002b85fc234c6f5f
RinkiMandal <rinkimandal944@gmail.com>
2026-02-16 13:35:06 +0530

    media changed

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,13 +1,39 @@
 import type { CollectionConfig } from 'payload'
-
+import path from 'path'
 export const Media: CollectionConfig = {
   slug: 'media',
-  upload: true,
-  fields: [
-    {
-      name: 'alt',
-      type: 'text',
-      required: false,
-    },
-  ],
+  access: {
+    read: () => true,
+  },
+  admin: {
+    hidden: false,
+  },
+
+  upload: {
+    staticDir: path.resolve('/var/www/pesino-media'),
+    mimeTypes: ['image/*'],
+
+    imageSizes: [
+      {
+        name: 'thumbnail',
+        width: 300,
+        height: 300,
+        crop: 'center',
+      },
+      {
+        name: 'medium',
+        width: 800,
+        height: 600,
+        crop: 'center',
+      },
+      {
+        name: 'large',
+        width: 1200,
+        height: 800,
+        crop: 'center',
+      },
+    ],
+  },
+
+  fields: [],
 }
--- src/payload-types.ts
+++ src/payload-types.ts
@@ -264,7 +264,6 @@ export interface Post {
  */
 export interface Media {
   id: number;
-  alt?: string | null;
   updatedAt: string;
   createdAt: string;
   url?: string | null;
@@ -276,6 +275,32 @@ export interface Media {
   height?: number | null;
   focalX?: number | null;
   focalY?: number | null;
+  sizes?: {
+    thumbnail?: {
+      url?: string | null;
+      width?: number | null;
+      height?: number | null;
+      mimeType?: string | null;
+      filesize?: number | null;
+      filename?: string | null;
+    };
+    medium?: {
+      url?: string | null;
+      width?: number | null;
+      height?: number | null;
+      mimeType?: string | null;
+      filesize?: number | null;
+      filename?: string | null;
+    };
+    large?: {
+      url?: string | null;
+      width?: number | null;
+      height?: number | null;
+      mimeType?: string | null;
+      filesize?: number | null;
+      filename?: string | null;
+    };
+  };
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
@@ -857,7 +882,6 @@ export interface PostsSelect<T extends boolean = true> {
  * via the `definition` "media_select".
  */
 export interface MediaSelect<T extends boolean = true> {
-  alt?: T;
   updatedAt?: T;
   createdAt?: T;
   url?: T;
@@ -869,6 +893,40 @@ export interface MediaSelect<T extends boolean = true> {
   height?: T;
   focalX?: T;
   focalY?: T;
+  sizes?:
+    | T
+    | {
+        thumbnail?:
+          | T
+          | {
+              url?: T;
+              width?: T;
+              height?: T;
+              mimeType?: T;
+              filesize?: T;
+              filename?: T;
+            };
+        medium?:
+          | T
+          | {
+              url?: T;
+              width?: T;
+              height?: T;
+              mimeType?: T;
+              filesize?: T;
+              filename?: T;
+            };
+        large?:
+          | T
+          | {
+              url?: T;
+              width?: T;
+              height?: T;
+              mimeType?: T;
+              filesize?: T;
+              filename?: T;
+            };
+      };
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
