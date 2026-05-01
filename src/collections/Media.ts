a4b0995b5b60cdd3b5e3e1c8ef4cd3607d8b4352
Shawn <shawn9920@gmail.com>
2026-05-01 18:33:18 +0530

    adjusted the path

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,13 +1,20 @@
 import type { CollectionConfig } from 'payload'
+import path from 'path'
 
 export const Media: CollectionConfig = {
   slug: 'media',
-  upload: true,
-  fields: [
-    {
-      name: 'alt',
-      type: 'text',
-      required: false,
-    },
-  ],
+  access: {
+    read: () => true,
+  },
+  admin: {
+    hidden: true,
+  },
+  upload: {
+    staticDir: path.resolve('/var/www/pesino-media'), // For linux
+    // staticDir: path.resolve('F:\Shawn\pbrs-uploads'), // For windows
+    // staticDir: 'media', // Folder where files will be stored
+    //  staticURL: '/media/images',
+    mimeTypes: ['image/*'], // Allow images & videos
+  },
+  fields: [],
 }
