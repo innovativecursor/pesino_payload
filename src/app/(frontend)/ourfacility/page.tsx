81b7057a70c71cb5d95f0b83d8ac7079bb775758
Shawn <shawn9920@gmail.com>
2026-05-20 11:10:42 +0530

    Changes to fix the image not being displayed

--- src/app/(frontend)/ourfacility/page.tsx
+++ src/app/(frontend)/ourfacility/page.tsx
@@ -24,6 +24,9 @@ export const metadata: Metadata = {
   },
 }
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export default async function Aboutus() {
   const payload = await getPayload({
     config,
